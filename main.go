098a0cf897ef4e8721a9ba314482951a848b5bca
parsaeissazadeh <parsa.eissazadeh@snapp.cab>
2023-01-19 13:18:29 +0330

    add a simple response for http server

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/labstack/echo/v4"
+)
+
+type Server struct {
+	e *echo.Echo
+}
+
+func main() {
+	s := &Server{}
+	s.e = echo.New()
+
+	s.e.GET("/", hello())
+
+	if err := s.e.Start("localhost:8080"); err != nil && err != http.ErrServerClosed {
+		s.e.Logger.Fatal("shutting down the server")
+	}
+}
+
+func hello() func(ctx echo.Context) error {
+	return func(ctx echo.Context) error {
+		return ctx.HTML(http.StatusOK, "<strong>Hello, World!</strong>")
+	}
 }
